825eb95ae6a45daab0753b270bd6e8dba1623e1c
Michael Lundquist <michaellundquist7@gmail.com>
2018-04-07 18:02:26 -0400

    reverting back to the way it was

--- problem1/prob1Universe.py
+++ problem1/prob1Universe.py
@@ -2,7 +2,6 @@ import re
 
 lines = int(input())
 for line in range(lines):
-    charges = 0
     cur_shot = 1
     damage = 0
     shots = []
@@ -11,20 +10,12 @@ for line in range(lines):
     minimum = int(minimum)
     #print(i)
     lastStart = -1
-    for letter in re.finditer("C",letters):
-        thisStart = letter.start()
-        deltaShots = (thisStart - lastStart - 1)#charges in a row?
-        damage += cur_shot * deltaShots
-        shots += ([cur_shot,] * deltaShots)
-        cur_shot = cur_shot << 1
-        charges += 1
-        lastStart = thisStart
-    #add for shots at the end of the line here
-    thisStart = len(letters)
-    deltaShots = (thisStart - lastStart - 1)#charges in a row?
-    damage += cur_shot * deltaShots
-    shots += ([cur_shot,] * deltaShots)
-    lastStart = thisStart
+    for letter in letters:
+        if letter == "S":
+            damage += cur_shot
+            shots.append(k)
+        else:
+            damage = damage << 1
 
     #performs swaps
     """
