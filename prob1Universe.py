f3e074c4a0900049d408e49ea9ade3d13a6a0478
Michael Lundquist <michaellundquist7@gmail.com>
2018-04-07 10:17:48 -0400

    converting to regex for creating the shots list

--- prob1Universe.py
+++ prob1Universe.py
@@ -1,18 +1,29 @@
+import re
 
 lines = int(input())
 for line in range(0,lines):
-    k = 1
-    t = 0
-    s = []
+    charges = 0
+    cur_shot = 1
+    damage = 0
+    shots = []
     i = input()
     minimum, letters = i.split(" ")
     print(i)
-    for letter in letters:
-        if letter == "S":
-            t += k
-            s.append(k)
-        else:
-            k = k << 1
-            print(k)
-    print(s, minimum)
+    lastStart = -1
+    for letter in re.finditer("C",letters):
+        thisStart = letter.start()
+        deltaShots = (thisStart - lastStart - 1)#charges in a row?
+        damage += cur_shot * deltaShots
+        shots += ([cur_shot,] * deltaShots)
+        cur_shot = cur_shot << 1
+        charges += 1
+        lastStart = thisStart
+    thisStart = len(letters)
+    deltaShots = (thisStart - lastStart - 1)#charges in a row?
+    damage += cur_shot * deltaShots
+    shots += ([cur_shot,] * deltaShots)
+    lastStart = thisStart    
+    #add for shots at the end of the line here
+    print(shots)
+    
         
