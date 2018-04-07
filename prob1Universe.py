060fb91ffd0cba2eefd7103c918d70218519d847
Michael Lundquist <michaellundquist7@gmail.com>
2018-04-07 10:36:52 -0400

    first attempt at performing the swaps

--- prob1Universe.py
+++ prob1Universe.py
@@ -18,12 +18,20 @@ for line in range(0,lines):
         cur_shot = cur_shot << 1
         charges += 1
         lastStart = thisStart
+    #add for shots at the end of the line here
     thisStart = len(letters)
     deltaShots = (thisStart - lastStart - 1)#charges in a row?
     damage += cur_shot * deltaShots
     shots += ([cur_shot,] * deltaShots)
-    lastStart = thisStart    
-    #add for shots at the end of the line here
+    lastStart = thisStart
+
+    #performs swaps
     print(shots)
+    swaps = 0
+    while (damage > minimum) and (shots[-1] > 1):
+        damage -= (shots[-1]-1)
+        del(shots[-1])
+        swaps += 1
+    
     
         
